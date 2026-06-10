453010ce07f0d82abdacad17994ee4b9fd720b60
smillingidiot <mariamlataria123@gmail.com>
2026-06-10 12:42:45 +0400

    added photos

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function showMessage() {
+    alert("Thank you for visiting my portfolio!");
+}
+function toggleMenu() {
+    document
+        .getElementById("nav-menu")
+        .classList.toggle("active");
+}
